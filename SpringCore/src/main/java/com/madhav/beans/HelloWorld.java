10c092f9fec51492ee3c6e3d4daafa91982671f8
madhava reddy <madhavaster@gmail.com>
2018-02-09 11:48:55 +0530

    small changes.

--- SpringCore/src/main/java/com/madhav/beans/HelloWorld.java
+++ SpringCore/src/main/java/com/madhav/beans/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.mkyong.common;
+package com.madhav.beans;
 
 public class HelloWorld {
 	private String name;
--- SpringCore/src/main/java/com/madhav/client/App.java
+++ SpringCore/src/main/java/com/madhav/client/App.java
@@ -1,13 +1,13 @@
-package com.mkyong.common;
+package com.madhav.client;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.madhav.beans.HelloWorld;
+
 public class App {
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext(
-				"spring.xml");
-
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		HelloWorld obj = (HelloWorld) context.getBean("helloBean");
 		obj.printHello();
 	}
