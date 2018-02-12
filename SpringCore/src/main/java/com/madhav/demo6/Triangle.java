8469c0980431e0e0d10b2d888577fdc9d095ccad
madhava reddy <madhavaster@gmail.com>
2018-02-12 21:53:53 +0530

    Initializing Collections example

---
+++ SpringCore/src/main/java/com/madhav/demo6/Triangle.java
@@ -0,0 +1,23 @@
+package com.madhav.demo6;
+
+import java.util.List;
+
+public class Triangle{
+	
+	private List<Point> points;
+
+	public List<Point> getPoints() {
+		return points;
+	}
+
+	public void setPoints(List<Point> points) {
+		this.points = points;
+	}
+
+	public void draw() {
+		
+		for(Point p: points) {
+			System.out.println(p.getX()+"...."+p.getY());
+		}
+	}
+}
