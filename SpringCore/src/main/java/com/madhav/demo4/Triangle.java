2d1e99ec25437743b40fe3bf0157ed1d0443e97f
madhava reddy <madhavaster@gmail.com>
2018-02-12 20:40:49 +0530

    constructor dependancy injection example 2

--- SpringCore/src/main/java/com/madhav/demo4/Triangle.java
+++ SpringCore/src/main/java/com/madhav/demo4/Triangle.java
@@ -3,11 +3,13 @@ package com.madhav.demo4;
 public class Triangle{
 
 	private String type;
-	public Triangle(String type) {
+	private int height;
+	public Triangle(String type,int height) {
 		this.type = type;
+		this.height = height;
 	}
 
 	public void draw() {
-		System.out.println(this.type+" triangle draw method is called");
+		System.out.println(this.type+" triangle is drawn with height "+this.height);
 	}
 }
