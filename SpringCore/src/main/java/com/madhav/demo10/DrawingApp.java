85bbb03b0070b40a3f8949b70860318b969d9af0
madhava reddy <madhavaster@gmail.com>
2018-02-12 23:44:25 +0530

    abstract bean , list merge option example incase of collections.

--- SpringCore/src/main/java/com/madhav/demo10/DrawingApp.java
+++ SpringCore/src/main/java/com/madhav/demo10/DrawingApp.java
@@ -8,6 +8,27 @@ public class DrawingApp {
 	public static void main(String[] args) {
 		ApplicationContext context= new ClassPathXmlApplicationContext("spring10.xml");
 		Triangle bean = context.getBean("triangle", Triangle.class);
+		System.out.println(bean.hashCode());
 		bean.draw();
+		Triangle bean2 = context.getBean("parenttriangle",Triangle.class);
+		System.out.println(bean2.hashCode());
 	}
 }
+
+/*
+ * Feb 12, 2018 11:43:09 PM org.springframework.context.support.AbstractApplicationContext prepareRefresh
+INFO: Refreshing org.springframework.context.support.ClassPathXmlApplicationContext@45ff54e6: startup date [Mon Feb 12 23:43:09 IST 2018]; root of context hierarchy
+Feb 12, 2018 11:43:09 PM org.springframework.beans.factory.xml.XmlBeanDefinitionReader loadBeanDefinitions
+INFO: Loading XML bean definitions from class path resource [spring10.xml]
+836514715
+0.....0
+10.....0
+0.....10
+Exception in thread "main" org.springframework.beans.factory.BeanIsAbstractException: Error creating bean with name 'parenttriangle': Bean definition is abstract
+	at org.springframework.beans.factory.support.AbstractBeanFactory.checkMergedBeanDefinition(AbstractBeanFactory.java:1319)
+	at org.springframework.beans.factory.support.AbstractBeanFactory.doGetBean(AbstractBeanFactory.java:293)
+	at org.springframework.beans.factory.support.AbstractBeanFactory.getBean(AbstractBeanFactory.java:205)
+	at org.springframework.context.support.AbstractApplicationContext.getBean(AbstractApplicationContext.java:1091)
+	at com.madhav.demo10.DrawingApp.main(DrawingApp.java:13)
+output
+ */
--- SpringCore/src/main/java/com/madhav/demo10/Triangle.java
+++ SpringCore/src/main/java/com/madhav/demo10/Triangle.java
@@ -1,43 +1,19 @@
 package com.madhav.demo10;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import java.util.List;
 
 public class Triangle{
 	
-	private Point pointA;
-	private Point pointB;
-	private Point pointC;
-
-	public Point getPointB() {
-		return pointB;
+	private List<Point> points;
+	public List<Point> getPoints() {
+		return points;
 	}
-
-	public void setPointB(Point pointB) {
-		this.pointB = pointB;
+	public void setPoints(List<Point> points) {
+		this.points = points;
 	}
-
-	public Point getPointC() {
-		return pointC;
-	}
-
-	public void setPointC(Point pointC) {
-		this.pointC = pointC;
-	}
-
-	public Point getPointA() {
-		return pointA;
-	}
-
-	public void setPointA(Point pointA) {
-		this.pointA = pointA;
-	}
-
 	public void draw() {
-		System.out.println(getPointA().getX()+"...."+getPointA().getY());
-		System.out.println(getPointB().getX()+"...."+getPointB().getY());
-		System.out.println(getPointC().getX()+"...."+getPointC().getY());
+		for(Point p : points) {
+			System.out.println(p.getX()+"....."+p.getY());
+		}
 	}
 }
