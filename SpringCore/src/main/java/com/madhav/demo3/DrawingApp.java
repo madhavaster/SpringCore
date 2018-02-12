84cdaf12a174e317f90e4f422bd23ee57bcc3f1a
madhava reddy <madhavaster@gmail.com>
2018-02-12 20:22:23 +0530

    ApplicationContext (IOC container) simple example

---
+++ SpringCore/src/main/java/com/madhav/demo3/DrawingApp.java
@@ -0,0 +1,15 @@
+package com.madhav.demo3;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp {
+	
+	public static void main(String[] args) {
+		//Resource resource = new FileSystemResource("C:\\Users\\User\\git\\SpringCore\\SpringCore\\src\\main\\resources\\spring2.xml");
+		//BeanFactory factory= new XmlBeanFactory(resource);
+		ApplicationContext context= new ClassPathXmlApplicationContext("spring3.xml");
+		Triangle bean = context.getBean("triangle", Triangle.class);
+		bean.draw();
+	}
+}
