3d3192856c155e5a41b0fc332364f49e55dbd738
madhava reddy <madhavaster@gmail.com>
2018-02-13 00:14:12 +0530

    Lifecycle Callbacks example.

---
+++ SpringCore/src/main/java/com/madhav/demo11/DrawingApp.java
@@ -0,0 +1,26 @@
+package com.madhav.demo11;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp {
+	
+	public static void main(String[] args) {
+		AbstractApplicationContext context= new ClassPathXmlApplicationContext("spring11.xml");
+		context.registerShutdownHook();
+		Triangle bean = context.getBean("triangle", Triangle.class);
+		bean.draw();
+	}
+}
+
+/*Feb 13, 2018 12:12:32 AM org.springframework.context.support.AbstractApplicationContext prepareRefresh
+INFO: Refreshing org.springframework.context.support.ClassPathXmlApplicationContext@45ff54e6: startup date [Tue Feb 13 00:12:32 IST 2018]; root of context hierarchy
+Feb 13, 2018 12:12:32 AM org.springframework.beans.factory.xml.XmlBeanDefinitionReader loadBeanDefinitions
+INFO: Loading XML bean definitions from class path resource [spring11.xml]
+setType is called
+afterPropertiesSet is called
+draw is called
+destroy is called
+Feb 13, 2018 12:12:32 AM org.springframework.context.support.AbstractApplicationContext doClose
+INFO: Closing org.springframework.context.support.ClassPathXmlApplicationContext@45ff54e6: startup date [Tue Feb 13 00:12:32 IST 2018]; root of context hierarchy
+*/
---
+++ SpringCore/src/main/java/com/madhav/demo11/Triangle.java
@@ -0,0 +1,35 @@
+package com.madhav.demo11;
+
+import java.util.List;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class Triangle implements InitializingBean,DisposableBean{
+	
+	private String type;
+	
+	public String getType() {
+		System.out.println("getType is called");
+		return type;
+	}
+
+	public void setType(String type) {
+		System.out.println("setType is called");
+		this.type = type;
+	}
+
+	public void draw() {
+		System.out.println("draw is called");
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("destroy is called");
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("afterPropertiesSet is called");
+	}
+}
